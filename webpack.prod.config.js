226e143df2f5a7cee7885ec44831b89f169b2f7a
Nguyen Hong Duc <autoclickvn@gmail.com>
2024-11-29 13:29:31 +0700

    Update config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require('webpack').container;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -31,10 +32,25 @@ module.exports = {
         reactApp: 'reactApp@https://react-child-profile.vercel.app/remoteEntry.js',
         vueApp: 'vueApp@https://vue-child-profile.vercel.app/remoteEntry.js',
         angularApp: 'angularApp@https://angular-child-profile.vercel.app/remoteEntry.js'
+      },
+      shared: {
+        react: { singleton: true, eager: true, requiredVersion: '18.0.0' },
+        'react-dom': { singleton: true, eager: true, requiredVersion: '18.0.0' }
       }
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '.',
+          globOptions: {
+            ignore: ['**/index.html']
+          }
+        }
+      ]
     })
   ]
 };
