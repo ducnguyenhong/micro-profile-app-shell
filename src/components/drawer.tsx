dab1cdb562e9ca757f584330e98940634ee184ba
Nguyen Hong Duc <autoclickvn@gmail.com>
2024-12-05 17:05:23 +0700

    Update UI

---
+++ src/components/drawer.tsx
@@ -0,0 +1,79 @@
+import { clsx } from 'clsx';
+import React from 'react';
+
+const openClassNames = {
+  right: 'translate-x-0',
+  left: 'translate-x-0',
+  top: 'translate-y-0',
+  bottom: 'translate-y-0'
+};
+
+const closeClassNames = {
+  right: 'translate-x-full',
+  left: '-translate-x-full',
+  top: '-translate-y-full',
+  bottom: 'translate-y-full'
+};
+
+const classNames = {
+  right: 'inset-y-0 right-0',
+  left: 'inset-y-0 left-0',
+  top: 'inset-x-0 top-0',
+  bottom: 'inset-x-0 bottom-0'
+};
+
+interface Props {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  side?: 'left' | 'right' | 'top' | 'bottom';
+}
+
+const Drawer: React.FC<Props> = ({ open, setOpen, side = 'left' }) => {
+  return (
+    <div
+      id={`dialog-${side}`}
+      className="relative z-10"
+      aria-labelledby="slide-over"
+      role="dialog"
+      aria-modal="true"
+      onClick={() => setOpen(!open)}
+    >
+      <div
+        className={clsx(
+          'fixed inset-0 bg-gray-500 bg-opacity-75 transition-all',
+          {
+            'opacity-100 duration-500 ease-in-out visible': open
+          },
+          { 'opacity-0 duration-500 ease-in-out invisible': !open }
+        )}
+      ></div>
+      <div className={clsx({ 'fixed inset-0 overflow-hidden': open })}>
+        <div className="absolute inset-0 overflow-hidden">
+          <div className={clsx('pointer-events-none fixed max-w-full', classNames[side])}>
+            <div
+              className={clsx(
+                'pointer-events-auto relative w-full h-full transform transition ease-in-out duration-500',
+                { [closeClassNames[side]]: !open },
+                { [openClassNames[side]]: open }
+              )}
+              onClick={(event) => {
+                event.preventDefault();
+                event.stopPropagation();
+              }}
+            >
+              <div
+                className={clsx(
+                  'flex flex-col h-full overflow-y-scroll bg-white p-20 shadow-xl bg-blue-400 rounded-lg'
+                )}
+              >
+                content
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Drawer;
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Drawer from '../components/drawer';
 import { IconFacebook, IconGithub, IconLinkedin, IconMoon, IconSun } from '../icons/social-icons';
 
 const Header: React.FC = () => {
   const [darkMode, setDarkMode] = useState<boolean>(true);
+  const [openLeft, setOpenLeft] = useState(false);
 
   const MENU_LIST = [
     {
@@ -39,52 +41,70 @@ const Header: React.FC = () => {
   ];
 
   return (
-    <nav className="header w-full h-[120px] flex items-center justify-between px-28">
-      <Link to="/" className="flex items-center gap-3">
-        <img src="/images/logo.png" className="w-12 h-12" />
-        <h1 className="text-white text-[22px] font-bold">Profile</h1>
-      </Link>
-      <ul className="flex items-center gap-16">
-        {MENU_LIST.map((item) => {
-          const { title, to, icon } = item;
-          return (
-            <li key={to} className="flex items-center gap-2">
-              <img src={icon} className="h-4 h-w object-cover" />
-              <Link to={to} className="group flex flex-col gap-[1px]">
-                <span className="text-white font-semibold text-[16px]">{title}</span>
-                <div className="flex justify-center">
-                  <div className="w-[0px] h-[1px] bg-gradient-to-r from-[#56b617] to-[#068414] group-hover:w-full duration-300" />
-                </div>
-              </Link>
-            </li>
-          );
-        })}
-
-        <div className="flex items-center gap-3">
-          {SOCIAL_LIST.map((social) => {
-            const { icon, href } = social;
+    <header>
+      <nav className="hidden lg:flex header w-full h-[120px] items-center justify-between px-28">
+        <Link to="/" className="flex items-center gap-3">
+          <img src="/images/logo.png" className="w-12 h-12" />
+          <h1 className="text-white text-[22px] font-bold">Profile</h1>
+        </Link>
+        <ul className="flex items-center gap-16">
+          {MENU_LIST.map((item) => {
+            const { title, to, icon } = item;
             return (
-              <a
-                key={href}
-                href={href}
-                target="_blank"
-                className="w-7 h-7 rounded-md bg-[#e6e6e617] flex items-center justify-center hover:bg-[#e6e6e63d] duration-200"
-              >
-                {icon}
-              </a>
+              <li key={to} className="flex items-center gap-2">
+                <img src={icon} className="h-4 h-w object-cover" />
+                <Link to={to} className="group flex flex-col gap-[1px]">
+                  <span className="text-white font-semibold text-[16px]">{title}</span>
+                  <div className="flex justify-center">
+                    <div className="w-[0px] h-[1px] bg-gradient-to-r from-[#56b617] to-[#068414] group-hover:w-full duration-300" />
+                  </div>
+                </Link>
+              </li>
             );
           })}
 
-          <button
-            type="button"
-            className="w-7 h-7 rounded-md bg-[#e6e6e617] flex items-center justify-center hover:bg-[#e6e6e63d] duration-200"
-            onClick={() => setDarkMode((prev) => !prev)}
-          >
-            {darkMode ? <IconSun w="18px" h="18px" color="#e6e6e6" /> : <IconMoon w="16px" h="16px" color="#e6e6e6" />}
-          </button>
-        </div>
-      </ul>
-    </nav>
+          <div className="flex items-center gap-3">
+            {SOCIAL_LIST.map((social) => {
+              const { icon, href } = social;
+              return (
+                <a
+                  key={href}
+                  href={href}
+                  target="_blank"
+                  className="w-7 h-7 rounded-md bg-[#e6e6e617] flex items-center justify-center hover:bg-[#e6e6e63d] duration-200"
+                >
+                  {icon}
+                </a>
+              );
+            })}
+
+            <button
+              type="button"
+              className="w-7 h-7 rounded-md bg-[#e6e6e617] flex items-center justify-center hover:bg-[#e6e6e63d] duration-200"
+              onClick={() => setDarkMode((prev) => !prev)}
+            >
+              {darkMode ? (
+                <IconSun w="18px" h="18px" color="#e6e6e6" />
+              ) : (
+                <IconMoon w="16px" h="16px" color="#e6e6e6" />
+              )}
+            </button>
+          </div>
+        </ul>
+      </nav>
+
+      <nav className="flex lg:hidden h-[70px] bg-[#e6e6e62e] justify-between items-center px-4">
+        <Link to="/" className="flex items-center gap-3">
+          <img src="/images/logo.png" className="w-10 h-10" />
+        </Link>
+
+        <button type="button" onClick={() => setOpenLeft(true)}>
+          <img src="https://cdn-icons-png.flaticon.com/128/7711/7711100.png" className="w-8 h-8" />
+        </button>
+
+        <Drawer open={openLeft} side="left" setOpen={setOpenLeft} />
+      </nav>
+    </header>
   );
 };
 
